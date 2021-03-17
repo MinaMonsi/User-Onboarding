69822b1d97b135bba21617b6718c001784b4ad5c
Marina Monsivais <minamonsi@me.com>
2021-03-17 00:58:08 -0500

    Added schema and effect hook

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,15 +1,25 @@
 import React, {useState} from 'react';
+import * as yup from 'yup'
 
 import './App.css';
 import Form from './components/Form';
 
-function App() {
-  const [form, setForm] = useState
-  return (
-    <div className="App">
-      
+// const initialFormValues = {
+//   //TEXT INPUTS
+//   user: '',
+//   email: '',
+//   password: '',
+//   //CHECKBOX
+//   agree: false,
+// }
+
+function App(){
+  return(
+    <div class="formDiv">
+      <Form/>
     </div>
-  );
+  )
 }
+    
 
 export default App;
